d80ec0249894a2e8994f97b1358aa62450c23da8
softwareengineering <softwareengineering@ubuntu>
2012-10-14 22:47:17 -0400

    Not much here, just committing for the night

---
+++ src/main/java/consumers/GenesConsumed.java
@@ -0,0 +1,23 @@
+package consumers;
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.collection.CasConsumer_ImplBase;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceProcessException;
+
+public class GenesConsumed extends CasConsumer_ImplBase {
+
+  @Override
+  public void processCas(CAS aCAS/*arg0*/) throws ResourceProcessException {
+    
+    JCas jcas;
+    try {
+      jcas = aCAS.getJCas();
+    } catch (CASException e) {
+      throw new ResourceProcessException(e);
+    }
+
+  }
+
+}
